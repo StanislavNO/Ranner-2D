28e88f48c56ccfb7f1627f89318ae6ade4bdc0b4
Stanislav N <stanislav_developer@mail.ru>
2023-11-27 03:49:14 +0500

    +

---
+++ Assets/Scripts/UI/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Parallax : MonoBehaviour
+    {
+        [SerializeField] private RawImage _image;
+        [SerializeField] private float _speed;
+
+        private float _fadeTime;
+
+        private void Update()
+        {
+            _fadeTime += _speed * Time.deltaTime;
+            _image.uvRect = new(_fadeTime,0,_image.uvRect.width, _image.uvRect.height);
+        }
+    }
+}
